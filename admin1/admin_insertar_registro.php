4fb3c34be80edf01ed62bd52696a2522d3d3e1aa
Juan Pablo Tapeosi Chamo <52146627+juanpablo45875785@users.noreply.github.com>
2024-04-04 12:23:16 -0400

    2

--- admin1/admin_insertar_registro.php
+++ admin1/admin_insertar_registro.php
@@ -6,9 +6,8 @@ if (
     isset($_POST['nombre_cont'], $_POST['apellido_cont'], $_POST['cod_car_1'], $_POST['cod_car_2'], $_POST['telefono_1'], $_POST['telefono_2'], $_POST['propietario_cont'], $_POST['cod_origen_dato'], $_POST['cod_estado_cont'], $_POST['descripcion_cont'], $_POST['estado_cont'])
     && !empty($_POST['nombre_cont']) && !empty($_POST['apellido_cont']) && !empty($_POST['cod_car_1']) && !empty($_POST['cod_car_2']) 
     && !empty($_POST['telefono_1']) && !empty($_POST['telefono_2']) && !empty($_POST['propietario_cont']) && !empty($_POST['cod_origen_dato']) 
-    && !empty($_POST['cod_estado_cont']) && !empty($_POST['descripcion_cont'])&& !empty($_POST['estado_cont']))
-
- {
+    && !empty($_POST['cod_estado_cont']) && !empty($_POST['descripcion_cont']) && !empty($_POST['estado_cont']))
+{
     // Obtener los datos del formulario y escaparlos para prevenir inyecciones SQL
     $nombre = mysqli_real_escape_string($conexion, $_POST['nombre_cont']);
     $apellido = mysqli_real_escape_string($conexion, $_POST['apellido_cont']);
@@ -20,19 +19,18 @@ if (
     $origen = mysqli_real_escape_string($conexion, $_POST['cod_origen_dato']);
     $codigoestado = mysqli_real_escape_string($conexion, $_POST['cod_estado_cont']);
     $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion_cont']);
-    $estado= mysqli_real_escape_string($conexion, $_POST['estado_cont']);
+    $estado = mysqli_real_escape_string($conexion, $_POST['estado_cont']);
 
-    // Verificar si el correo o carnet ya existen en la base de datos
-    $verif = "SELECT * FROM tbl_contacto WHERE telefono_1= '$telefono1'";// or si hay algun registro no repetible
-    $resultado = mysqli_query($conexion, $verif);
+    // Insertar el nuevo propietario
+    $sql_insert_propietario = "INSERT INTO propietario (nombre) VALUES ('$propietario')";
+    $query_insert_propietario = mysqli_query($conexion, $sql_insert_propietario);
 
-    // Si el correo o carnet ya existe, redirigir al formulario de registro con un mensaje de error
-    if (mysqli_num_rows($resultado) > 0) {
-        header('Location: admin.php?error=nombre_cont_apellido_cont_existente');
-        exit;
-    } else {
+    if ($query_insert_propietario) {
+        // Obtener el ID del propietario recién insertado
+        $id_propietario = mysqli_insert_id($conexion);
+        
         // Preparar la consulta para insertar el nuevo usuario
-        $sql = "INSERT INTO tbl_contacto (nombre_cont, apellido_cont, cod_car_1, cod_car_2, telefono_1, telefono_2, propietario_cont, cod_origen_dato, cod_estado_cont, descripcion_cont, estado_cont) VALUES ('$nombre', '$apellido', '$codigo1', '$codigo2', '$telefono1', '$telefono2', '$propietario', '$origen', '$codigoestado', '$descripcion', $estado)";
+        $sql = "INSERT INTO tbl_contacto (nombre_cont, apellido_cont, cod_car_1, cod_car_2, telefono_1, telefono_2, propietario_cont, cod_origen_dato, cod_estado_cont, descripcion_cont, estado_cont) VALUES ('$nombre', '$apellido', '$codigo1', '$codigo2', '$telefono1', '$telefono2', '$id_propietario', '$origen', '$codigoestado', '$descripcion', '$estado')";
 
         // Ejecutar la consulta
         $query = mysqli_query($conexion, $sql);
@@ -46,6 +44,9 @@ if (
             // Si hay un error en la inserción, mostrar un mensaje
             echo "ERROR AL INSERTAR";
         }
+    } else {
+        // Si hay un error al insertar el propietario, mostrar un mensaje de error
+        echo "Error al insertar el propietario en la base de datos.";
     }
 } else {
     // Si no se recibieron los datos del formulario o algún campo está vacío, redirigir al formulario de registro con un mensaje de error
