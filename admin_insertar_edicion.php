43f42f15f2c5297be15866f334002b00817ffeb6
Juan Pablo Tapeosi Chamo <52146627+juanpablo45875785@users.noreply.github.com>
2024-04-11 12:34:07 -0400

    Add files via upload

--- admin_insertar_edicion.php
+++ admin_insertar_edicion.php
@@ -2,26 +2,68 @@
 include("conexion.php");
 
 // Verificar si se envió el formulario y si los datos están presentes
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nombre_cont'], $_POST['apellido_cont'], $_POST['cod_car_1'], $_POST['cod_car_2'], $_POST['telefono_1'], $_POST['telefono_2'], $_POST['propietario_cont'], $_POST['cod_origen_dato'], $_POST['cod_estado_cont'], $_POST['descripcion_cont'], $_POST['estado_cont'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_contacto'])) {
 
-    // Obtener los datos del formulario y escaparlos para prevenir inyecciones SQL
-    $nombre = mysqli_real_escape_string($conexion, $_POST['nombre_cont']);
-    $apellido = mysqli_real_escape_string($conexion, $_POST['apellido_cont']);
-    $codigo1 = mysqli_real_escape_string($conexion, $_POST['cod_car_1']);
-    $codigo2 = mysqli_real_escape_string($conexion, $_POST['cod_car_2']);
-    $telefono1 = mysqli_real_escape_string($conexion, $_POST['telefono_1']);
-    $telefono2 = mysqli_real_escape_string($conexion, $_POST['telefono_2']);
-    $propietario = mysqli_real_escape_string($conexion, $_POST['propietario_cont']);
-    $origen = mysqli_real_escape_string($conexion, $_POST['cod_origen_dato']);
-    $codigoestado = mysqli_real_escape_string($conexion, $_POST['cod_estado_cont']);
-    $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion_cont']);
-    $estado = mysqli_real_escape_string($conexion, $_POST['estado_cont']);
-
-    // Obtener el ID del usuario a editar (supongamos que se pasa como un parámetro GET llamado 'id')
+    // Obtener el ID del contacto a editar
     $id = $_POST['id_contacto'];
 
-    // Preparar la consulta para actualizar los datos del usuario
-    $sql = "UPDATE tbl_contacto SET nombre_cont='$nombre', apellido_cont='$apellido', cod_car_1='$codigo1', cod_car_2='$codigo2', telefono_1='$telefono1', telefono_2='$telefono2', propietario_cont='$propietario', cod_origen_dato='$origen', cod_estado_cont='$codigoestado', descripcion_cont='$descripcion', estado_cont='$estado' WHERE id_contacto='$id'";
+    // Preparar la consulta para actualizar los datos del contacto
+    $sql = "UPDATE tbl_contacto SET ";
+
+    // Crear un array para almacenar los fragmentos de la consulta SET
+    $set_array = array();
+
+    // Verificar y añadir los campos que tienen valores
+    if (!empty($_POST['nombre_cont'])) {
+        $nombre = mysqli_real_escape_string($conexion, $_POST['nombre_cont']);
+        $set_array[] = "nombre_cont='$nombre'";
+    }
+    if (!empty($_POST['apellido_cont'])) {
+        $apellido = mysqli_real_escape_string($conexion, $_POST['apellido_cont']);
+        $set_array[] = "apellido_cont='$apellido'";
+    }
+    if (!empty($_POST['cod_car_1'])) {
+        $codigo1 = mysqli_real_escape_string($conexion, $_POST['cod_car_1']);
+        $set_array[] = "cod_car_1='$codigo1'";
+    }
+    if (!empty($_POST['cod_car_2'])) {
+        $codigo2 = mysqli_real_escape_string($conexion, $_POST['cod_car_2']);
+        $set_array[] = "cod_car_2='$codigo2'";
+    }
+    if (!empty($_POST['telefono_1'])) {
+        $telefono1 = mysqli_real_escape_string($conexion, $_POST['telefono_1']);
+        $set_array[] = "telefono_1='$telefono1'";
+    }
+    if (!empty($_POST['telefono_2'])) {
+        $telefono2 = mysqli_real_escape_string($conexion, $_POST['telefono_2']);
+        $set_array[] = "telefono_2='$telefono2'";
+    }
+    if (!empty($_POST['propietario_cont'])) {
+        $propietario = mysqli_real_escape_string($conexion, $_POST['propietario_cont']);
+        $set_array[] = "propietario_cont='$propietario'";
+    }
+    if (!empty($_POST['cod_origen_dato'])) {
+        $origen = mysqli_real_escape_string($conexion, $_POST['cod_origen_dato']);
+        $set_array[] = "cod_origen_dato='$origen'";
+    }
+    if (!empty($_POST['cod_estado_cont'])) {
+        $codigoestado = mysqli_real_escape_string($conexion, $_POST['cod_estado_cont']);
+        $set_array[] = "cod_estado_cont='$codigoestado'";
+    }
+    if (!empty($_POST['descripcion_cont'])) {
+        $descripcion = mysqli_real_escape_string($conexion, $_POST['descripcion_cont']);
+        $set_array[] = "descripcion_cont='$descripcion'";
+    }
+    if (!empty($_POST['estado_cont'])) {
+        $estado = mysqli_real_escape_string($conexion, $_POST['estado_cont']);
+        $set_array[] = "estado_cont='$estado'";
+    }
+
+    // Construir la parte SET de la consulta
+    $sql .= implode(", ", $set_array);
+
+    // Agregar la condición WHERE
+    $sql .= " WHERE id_contacto='$id'";
 
     // Ejecutar la consulta
     $query = mysqli_query($conexion, $sql);
@@ -36,8 +78,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nombre_cont'], $_POST[
         echo "ERROR AL ACTUALIZAR";
     }
 } else {
-    // Si no se enviaron los datos del formulario, redirigir al formulario de edición con un mensaje de error
+    // Si no se enviaron los datos del formulario o el ID del contacto, redirigir al formulario de edición con un mensaje de error
     header("Location: admin_editar.php?error=datos_incompletos");
     exit;
 }
 ?>
+
